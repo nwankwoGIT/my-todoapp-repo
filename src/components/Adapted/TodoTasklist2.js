cf7c869b0b25b4e33ecbe6b1819ad5238b8ae954
nwankwoGIT <61326912+nwankwoGIT@users.noreply.github.com>
2020-06-04 15:35:59 -0500

    Update TodoTasklist2.js

--- src/components/Adapted/TodoTasklist2.js
+++ src/components/Adapted/TodoTasklist2.js
@@ -10,24 +10,24 @@ import {Card, Divider,Col, Row} from 'antd'
 export const TodoTasklist2 = () => { 
     const gridHeaderStyle = {       
         width: '15%',
-        textAlign: 'center',
+        textAlign: 'left',
         color:"red",
         fontWeight: "bold"
       };
       const gridHeaderStyle2 = {       
         width: '25%',
-        textAlign: 'center',
+        textAlign: 'left',
         color:"red",
         fontWeight: "bold"
       };
       const gridStyle = {       
         width: '15%',
-        textAlign: 'center',
-        objectFit: 'scale-down'
+        textAlign: 'left',
+       
       };
       const gridStyle2 = {       
         width: '25%',
-        textAlign: 'center',
+        textAlign: 'left',
 
       };
     const { todos, removeTodo, editTodo,togglecompleteTodo} = useContext(TodoGlobalContext);         
@@ -53,7 +53,7 @@ export const TodoTasklist2 = () => {
                 <Card.Grid style={gridStyle}><p style={{textDecoration: todotask.iscomplete ? 'line-through' : 'none'}}>{todotask.status}</p></Card.Grid>
                 <Card.Grid style={gridStyle}>                    
                 <Link to={`/edit/${todotask.id}`}>
-                                <button onClick={() => editTodo(todotask.id)} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold mr-3 py-2 px-2 rounded-full inline-flex items-center">
+                                <button style={{ objectFit: 'scale-down'}} onClick={() => editTodo(todotask.id)} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold mr-3 py-2 px-2 rounded-full inline-flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit">
                                         <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                                         <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
@@ -64,13 +64,13 @@ export const TodoTasklist2 = () => {
                     </Card.Grid>
                 <Card.Grid style={gridStyle}>    
                  
-                 <button onClick={() => togglecompleteTodo(todotask.id)} className="block bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-2 rounded-full inline-flex items-center">
+                 <button style={{ objectFit: 'scale-down'}} onClick={() => togglecompleteTodo(todotask.id)} className="block bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-2 rounded-full inline-flex items-center">
                         <FontAwesomeIcon icon={faToggleOn} />
                      </button> 
                     
                     </Card.Grid>    
                 <Card.Grid style={gridStyle}>
-                <button onClick={() => removeTodo(todotask.id)} className="block bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-2 rounded-full inline-flex items-center">
+                <button style={{ objectFit: 'scale-down'}} onClick={() => removeTodo(todotask.id)} className="block bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-2 rounded-full inline-flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2">
                                     <polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
                                     <line x1="10" y1="11" x2="10" y2="17"></line>
