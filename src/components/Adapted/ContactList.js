baf1089b68fb8025744ad924b6dd1af454ff2946
nwankwoGIT <61326912+nwankwoGIT@users.noreply.github.com>
2020-06-04 22:24:20 -0500

    more edits

--- src/components/Adapted/ContactList.js
+++ src/components/Adapted/ContactList.js
@@ -14,9 +14,9 @@ export const ContactList = () => {
     const {contacts, removeContact } = useContext(TodoGlobalContext);          
        
         return (
-            <>                            
-            <div style={{width:"100vw"}}>               
-            <TodoMenu/>     
+            <>   
+            <TodoMenu/>               
+            <div>                                             
             <h2 className="text-center">List Of All Contacts </h2>             
             <Table style={{ width: '100%', height: 'auto' }} dataSource={contacts} rowKey={(record) => record.id} >               
                 <Column title="First Name" dataIndex="firstname" key="firstname" width='16vw'/>
@@ -24,19 +24,19 @@ export const ContactList = () => {
                 <Column title="Email Address" dataIndex="email" key="email" width='16vw'/>
                 <Column title="Comments" dataIndex="comments" key="comments" width='16vw'/>                 
                 <Column
-                title="Delete"
-                key="action"
-                render={(text, record) => (
-                <Space size="middle">                    
-                    <button onClick={() => removeContact(record.id)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2">
-                                    <polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                                </svg>
-                     </button> 
-                </Space>
-                )}         
+                    title="Delete"
+                    key="action"
+                    render={(text, record) => (
+                    <Space size="middle">                    
+                        <button onClick={() => removeContact(record.id)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2">
+                                        <polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                                    </svg>
+                        </button> 
+                    </Space>
+                    )}         
                 />
             </Table> 
             </div>
--- src/components/Adapted/TodoHome.js
+++ src/components/Adapted/TodoHome.js
@@ -6,10 +6,10 @@ import {ContactList} from './ContactList'
 
 export const TodoHome = () => {      
 return (  
-        <Fragment>            
+        <Fragment>    
+            <TodoMenu/>
             <div className="App">
-                 <div className="container mx-auto">
-                    <TodoMenu/>
+                 <div className="container mx-auto">                   
                     <h3 className="text-center  text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">Using React Context API, Router and Hooks</h3>                    
                     <TodoHeading />
                     <TodoTasklist2 /> 
